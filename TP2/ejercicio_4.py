76d91f03e843e625fb35bfcee14e781d6b85b346
Aron L. <aronlovey@gmail.com>
2024-09-18 00:40:38 -0300

    cosas nuevas

--- TP2/ejercicio_4.py
+++ TP2/ejercicio_4.py
@@ -1,7 +1,7 @@
 """
-Eliminar de una lista de números enteros aquellos valores que se encuentren en 
-una segunda lista. Imprimir la lista original, la lista de valores a eliminar y la lista 
-resultante. La función debe modificar la lista original sin crear una copia modificada.
+    Eliminar de una lista de números enteros aquellos valores que se encuentren en 
+    una segunda lista. Imprimir la lista original, la lista de valores a eliminar y la lista 
+    resultante. La función debe modificar la lista original sin crear una copia modificada.
 """
 
 def ingresar_numero_en_lista(numbers: list[int]):
