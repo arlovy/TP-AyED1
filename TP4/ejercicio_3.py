e6deacf2b0d69a05f1aed623ce558266e0c90766
Aron L. <aronlovey@gmail.com>
2024-09-19 00:23:05 -0300

    clavicula

--- TP4/ejercicio_3.py
+++ TP4/ejercicio_3.py
@@ -6,3 +6,26 @@ en posiciones impares de la clave maestra y la segunda con los dígitos ubicados
 posiciones  pares.  Los  dígitos  se  numeran  desde  la  izquierda.  Ejemplo:  Si  clave 
 maestra fuera 18293, la clave 1 sería 123 y la clave 2 sería 89.
 """
+
+def obtener_claves(clavicula: str):
+    """
+    Esta función recibe un string y lo divide en dos, uno con los caracteres en
+    posiciones pares y otro con caracteres en posiciones impares, y luego
+    los retorna en una tupla.
+    """
+    clavicula1 = ''
+    clavicula2 = ''
+
+    for i in range(len(clavicula)):
+        if i % 2 == 0:
+            clavicula1 += clavicula[i]
+        else:
+            clavicula2 += clavicula[i]
+
+    return clavicula1, clavicula2
+
+# Ejemplo de uso
+clave_maestra = int(input("Ingrese la clave maestra: "))
+clave1, clave2 = obtener_claves(clave_maestra)
+print("Clave 1:", clave1)
+print("Clave 2:", clave2)
