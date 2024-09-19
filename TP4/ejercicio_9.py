a2ba912a8e0e052f55eb2533e66e0d5d8068a428
Aron L. <aronlovey@gmail.com>
2024-09-19 10:54:41 -0300

    EJE9

--- TP4/ejercicio_9.py
+++ TP4/ejercicio_9.py
@@ -5,3 +5,24 @@ cadena  con  las  palabras  ordenadas  según  su  longitud,  dejando  un  espac
 cada  una.  Los  signos  de  puntuación  no  deben  ser  tenidos  en  cuenta  al  medir  la 
 longitud de las palabras, pero deberán conservarse en la cadena final.
 """
+
+def ordenar_longitud(string: str):
+    """
+    Esta función ordena las palabras de una cadena por longitud.
+    Recibe una cadena no vacía.
+    Retorna la cadena con las palabras ordenadas por longitud.
+    """
+    assert len(string) > 0
+
+    palabras = string.split()
+
+    def longitud_sin_puntuacion(palabra):
+        palabra_limpia = ''.join([char for char in palabra if char not in [".",",",";"]])
+        return len(palabra_limpia)
+
+    palabras_ordenadas = sorted(palabras, key=longitud_sin_puntuacion)
+
+    return ' '.join(palabras_ordenadas)
+
+ingreso_cadena = input("Ingrese la cadena: ")
+print(ordenar_longitud(ingreso_cadena))
