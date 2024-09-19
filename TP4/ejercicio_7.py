7f9c85c67be389260d0b5db12212ef847d1f8ddc
Aron L. <aronlovey@gmail.com>
2024-09-19 10:34:25 -0300

    eje7

--- TP4/ejercicio_7.py
+++ TP4/ejercicio_7.py
@@ -6,3 +6,47 @@ Escribir una función para cada uno de los siguientes casos:
     a. Utilizando rebanadas
     b. Sin utilizar rebanadas
 """
+
+
+def eliminar_subcadena_rebanada(cadena: int, posicion: int, cantidad: int):
+    """
+    Esta función elimina una sección de una cadena.
+    Recibe una cadena, un entero que determina la posición inicial, y una
+    cantidad de caracteres a eliminar.
+    Retorna una nueva cadena.
+    """
+    return cadena[:posicion] + cadena[posicion + cantidad :]
+
+
+def eliminar_subcadena_sin_rebanada(cadena: int, posicion: int, cantidad: int):
+    """
+    Esta función elimina una sección de una cadena.
+    Recibe una cadena, un entero que determina la posición inicial, y una
+    cantidad de caracteres a eliminar.
+    Retorna una nueva cadena.
+    """
+    nueva_cadena = ""
+
+    for i in range(len(cadena)):
+        if i < posicion or i >= posicion + cantidad:
+            nueva_cadena += cadena[i]
+
+    return nueva_cadena
+
+
+if __name__ == "__main__":
+    string = input("Ingrese el string: ")
+    try:
+        if string:
+            posicion_inicial = int(input("Ingrese la posición inicial: "))
+            caracteres = int(input("Ingrese la cantidad de caracteres a rebanar: "))
+            salida1 = eliminar_subcadena_rebanada(string, posicion_inicial, caracteres)
+            salida2 = eliminar_subcadena_sin_rebanada(
+                string, posicion_inicial, caracteres
+            )
+            print(f"Subcadena extraída (con rebanadas): '{salida1}'")
+            print(f"Subcadena extraida (sin rebanadas): '{salida2}'")
+        else:
+            print("El string está vacio.")
+    except ValueError:
+        print("Ingrese un entero.")
