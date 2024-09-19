51e8c10dd70c65126ae41fd10c71cf30ec271a7f
Aron L. <aronlovey@gmail.com>
2024-09-19 11:17:51 -0300

    hola

--- TP4/ejercicio_12.py
+++ TP4/ejercicio_12.py
@@ -3,3 +3,7 @@ Escribir un programa para crear una lista por comprensión con los naipes de la 
 raja española. La lista debe contener cadenas de caracteres. Ejemplo: ["1 Oros", "2 
 Oros"... ]. Imprimir la lista por pantalla.
 """
+
+palos = ('Oros', 'Bastos', 'Espadas', 'Copas')
+barajas = [' '.join([str(x), y]) for y in palos for x in range(1, 13)]
+print(barajas)
