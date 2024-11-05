532658fd0a5cde25e0aa8b4769339dee0810b485
Aron L. <aronlovey@gmail.com>
2024-11-05 17:21:25 -0300

    hola mariano termien el ejercicio 4 del tp8

--- TP8/ejercicio_4.py
+++ TP8/ejercicio_4.py
@@ -4,3 +4,19 @@ son recibidas en dos tuplas, por ejemplo: (3, 4) y (5, 4). La función devuelve 
 o False. Escribir también un programa para verificar su comportamiento. Considerar 
 el uso de conjuntos para resolver este ejercicio.
 """
+def unir_fichas(ficha1:tuple, ficha2:tuple) -> bool:
+    '''
+    Compara dos tuplas de enteros y retorna si algun valor coincide entre ellas.
+    Recibe dos tuplas con enteros.
+    Retorna un booleano.
+    '''
+    return not set(ficha1).isdisjoint(set(ficha2))
+
+if __name__ == "__main__":
+    cosa1 = (3, 4)
+    cosa2 = (5, 4)
+
+    if unir_fichas(cosa1, cosa2):
+        print(f"Las fichas {cosa1} y {cosa2} encajan.")
+    else:
+        print(f"Las fichas {cosa1} y {cosa2} no encajan.")
