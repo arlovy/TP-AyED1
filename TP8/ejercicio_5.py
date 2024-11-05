b77fb4cc03c7a35a214cf9d297336e5942ff014b
Aron L. <aronlovey@gmail.com>
2024-11-05 17:34:47 -0300

    hola ejercicio 5

--- TP8/ejercicio_5.py
+++ TP8/ejercicio_5.py
@@ -12,3 +12,23 @@ Escribir una función que reciba dos vectores en forma de tuplas y devuelva un v
 de verdad indicando si son ortogonales o no. Desarrollar también un programa que 
 permita verificar el comportamiento de la función.
 """
+
+def is_ortogonal(vector1:tuple, vector2:tuple) -> int:
+    """
+    Recibe dos tuplas que representan los vértices de vectores y verifica
+    si son ortogonales.
+    Recibe dos tuplas con enteros, de la misma longitud.
+    Retorna un valor booleano.
+    """
+    return not bool(sum(a * b for a, b in zip(vector1, vector2)))
+
+
+if __name__ == "__main__":
+
+    cosa1 = (3, 4)
+    cosa2 = (-4, 3)
+
+    if is_ortogonal(cosa1, cosa2):
+        print(f"Los vectores {cosa1} y {cosa2} son ortogonales.")
+    else:
+        print(f"Los vectores {cosa1} y {cosa2} no son ortogonales.")
