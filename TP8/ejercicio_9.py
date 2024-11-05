6ff454ad5408dc32a6186ceb1c303e0cd7d0c378
Aron L. <aronlovey@gmail.com>
2024-11-05 19:37:17 -0300

    eje9

--- TP8/ejercicio_9.py
+++ TP8/ejercicio_9.py
@@ -3,3 +3,9 @@ Escribir  un  programa  que  permita  ingresar  un  número  entero  N  y  gener
 diccionario por comprensión con la tabla de multiplicar de N del 1 al 12. Mostrar la 
 tabla de multiplicar con el formato apropiado.
 """
+
+if __name__ == "__main__":
+    n = int(input("Ingrese un entero: "))
+    diccionario = {i: n * i for i in range(1, 13)}
+    for multiplo, resultado in diccionario.items():
+        print(f"{n} x {multiplo} = {resultado}")
