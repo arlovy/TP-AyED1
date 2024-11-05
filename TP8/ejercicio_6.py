44628396f73d3b4117a8e34433efb93523c4cc49
Aron L. <aronlovey@gmail.com>
2024-11-05 17:50:29 -0300

    si

--- TP8/ejercicio_6.py
+++ TP8/ejercicio_6.py
@@ -4,3 +4,9 @@ repetidas, dejando un solo ejemplar de cada una. Finalmente mostrar las palabras
 ordenadas  según  su  longitud.  Los  signos  de  puntuación  no  deben  afectar  el 
 proceso
 """
+
+if __name__ == "__main__":
+    frase = input("Ingrese su frase: ")
+    unicas = set(map(lambda x: x.replace(",","").replace(".","").capitalize(), frase.lower().split(" ")))
+    for num, elem in enumerate(sorted(unicas, key=lambda x: len(x)), start=1):
+        print(f"{num}.{elem}")
