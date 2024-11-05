34b4071153c4055727257f3a9badf7f7670a2ff8
Aron L. <aronlovey@gmail.com>
2024-11-05 19:33:02 -0300

    eje8

--- TP8/ejercicio_8.py
+++ TP8/ejercicio_8.py
@@ -2,3 +2,8 @@
 Generar e imprimir un diccionario donde las claves sean números enteros entre 1 y 
 20 (ambos incluidos) y los valores asociados sean el cuadrado de las claves.
 """
+
+if __name__ == "__main__":
+    dic_cuadrados = {num:num**2 for num in range(1,21)}
+
+    print(dic_cuadrados)
