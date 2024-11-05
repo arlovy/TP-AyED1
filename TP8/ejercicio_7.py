436ee9eb95874617d3cdc2119357ef95bbf1aa33
Aron L. <aronlovey@gmail.com>
2024-11-05 19:29:55 -0300

    eje7

--- TP8/ejercicio_7.py
+++ TP8/ejercicio_7.py
@@ -5,3 +5,29 @@ tenido  del  conjunto  luego  de  cada  eliminación.  Finalizar  el  proceso  a
 Utilizar  manejo  de  excepciones  para  evitar  errores  al  intentar  quitar  elementos 
 inexistentes.
 """
+
+def eliminar_numero(conjunto: set, num: int):
+    '''
+    Elimina un número determinado de un conjunto.
+    Recibe un conjunto de numeros enteros, y un entero a eliminar.
+    Retorna el conjunto sin el entero especificado.
+    '''
+    return conjunto.remove(num)
+
+
+if __name__ == "__main__":
+
+    myset = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+    while True:
+        try:
+            num_a_eliminar = int(input("Ingrese el numero a eliminar (-1 para salir): "))
+            if num_a_eliminar == -1:
+                print("Saliendo...")
+                break
+            print(eliminar_numero(myset, num_a_eliminar))
+
+        except KeyError:
+            print("El número a eliminar no existe en el conjunto.")
+        except ValueError:
+            print("Ingrese un número válido.")
