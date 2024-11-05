736efcb19dafe885fe0614c3e994e6cff8d6703a
Aron L. <aronlovey@gmail.com>
2024-11-05 20:26:50 -0300

    ejercicio10

--- TP8/ejercicio_10.py
+++ TP8/ejercicio_10.py
@@ -5,3 +5,27 @@ contenidas  en  la  lista,  devolviendo  el  diccionario  modificado  y  un  nú
 que represente la cantidad de claves eliminadas. Desarrollar también un programa 
 para verificar su comportamiento.
 """
+
+def eliminarclaves(dict_: dict, claves: list[str]) -> tuple:
+    """
+    Elimina claves y sus valores de un diccionario cualquiera.
+    Recibe un diccionario y una lista de strings que indican las claves.
+    Retorna una tupla (diccionario nuevo, cantidad de elementos eliminados).
+    """
+    eliminados = 0
+    for key in claves:
+        if key in dict_:
+            try:
+                dict_.pop(key)
+            except Exception:
+                return dict_, 0
+            eliminados += 1
+    return dict_, eliminados
+
+
+if __name__ == "__main__":
+    diccionario = {
+    "Nombre" : "Pedro",
+    "Apellido" : "Perez"
+    }
+    print(eliminarclaves(diccionario, ["Nombre"]))
