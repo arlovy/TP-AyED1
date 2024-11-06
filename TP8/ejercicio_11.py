f11f6b9be3baa3a2ee7fec3131451aedc6f1566c
Aron L. <aronlovey@gmail.com>
2024-11-06 17:44:56 -0300

    eje11

--- TP8/ejercicio_11.py
+++ TP8/ejercicio_11.py
@@ -5,3 +5,25 @@ resultados.  Luego  desarrollar  un  programa  para  leer  una  frase  e  invoca
 función  por  cada  palabra  que  contenga  la  misma.  Imprimir  las  palabras  y  la 
 cantidad de vocales hallada
 """
+
+def cuenta_vocales(palabra: str) -> dict:
+    """
+    Recibe un string y cuenta sus vocales.
+    Recibe un string.
+    Retorna un diccionario {vocal:cantidad}
+    """
+
+    vocales = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+
+    for char in palabra.lower():
+        if char in vocales: 
+            vocales[char] += 1 
+    return vocales
+
+
+if __name__ == "__main__":
+    palabra = "Juniors"
+    salida = cuenta_vocales(palabra)
+    print("Cantidad de vocales:")
+    for voc, cantidad in salida.items():
+        print(f"{voc.upper()}: {cantidad}")
